0c988fadd71de702f217be20942f9d9c484822fe
Suzanne <suzanne.repellin@gmail.com>
2020-05-25 12:03:59 +0200

    ex03

---
+++ ex03/count.py
@@ -0,0 +1,21 @@
+import re
+
+def text_analyzer(text="", *argv):
+    if len(argv) > 0:
+        print("ERROR")
+    elif text == "":
+        text = input("What is the text to analyze?\n")
+        text_analyzer(text)
+    else:
+        total_len = len(text)
+        #up_re = re.compile(r'[A-B]')
+        #down_re = re.compile(r'[a-b]')
+        up_len = len(re.findall(r'[A-Z]', text))
+        down_len = len(re.findall(r'[a-z]', text))
+        space_len = text.count(' ')
+        punc_len = len(re.findall(r"\.|,|:|'|&|\;|!|\?|\*|\(|\)|\-", text))
+        print('The text contains ', str(total_len), ' characters:')
+        print('- ', str(up_len), ' upper letters')
+        print('- ', str(down_len), ' lower letters')
+        print('- ', str(punc_len), ' punctuation marks')
+        print('- ', str(space_len), ' spaces')
